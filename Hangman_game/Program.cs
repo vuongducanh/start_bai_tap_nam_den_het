9d67df84f72fe2e505420068b6f7c5d3688880d4
vuongducanh <anhvuong730@gmail.com>
2018-02-01 11:22:23 +0700

    updater

---
+++ Hangman_game/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+namespace Hangman_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                
+                string value = "vinhphuc";
+                char[] array = value.ToCharArray();
+                int len = array.Length;
+                string len2 = "";
+                for (int i = 0; i < len; i++)
+                {
+                   len2+="_";
+                }
+                char[] len3 = len2.ToCharArray(); 
+                 Console.WriteLine(" you welcome");
+                int count=0;
+                while(true)
+                {       
+                    Console.Write(" nhap vao ki tu input = ");
+                    char input = Convert.ToChar(Console.ReadLine());
+                    for(int j=0;j<len;j++)
+                    {
+                        if(input == array[j])
+                        {
+                            len3[j] = array[j];
+                        } 
+                    }
+                     Console.WriteLine(len3);
+                    count++;
+                    
+                    var result = array.Except(len3);
+                    if(result.Count()==0)
+                    {
+                        Console.WriteLine("you win");
+                        break;
+                    }
+                    if(count == 7)
+                    {
+                        Console.WriteLine("game over");
+                        break;
+                    }
+                }
+            }
+            catch (System.FormatException)
+            {
+            System.Console.WriteLine("ki tu ban nhap khong hop le ");
+            }
+        }
+    }
+}
---
+++ bai_tap_6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bai_tap_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("chua co gi");
+        }
+    }
+}
